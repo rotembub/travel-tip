edbb86a9c6b24aca87aa418dcf024506c19f6fb5
rotem <rotembub@gmail.com>
2021-11-03 13:31:00 +0200

    minor addition

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -40,7 +40,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyCgVxgFrhelEWoUrYr9_O6ucbx63KpU8F8'; //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
